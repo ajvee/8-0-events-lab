07a84c869ff974a811c62dba25cc4d0eba5d3e14
Angel <EMAIL>
2022-12-13 20:21:24 -0500

    Events

--- main.js
+++ main.js
@@ -8,8 +8,20 @@ for (let i = 0; i < 100; i++) {
 
 // You may write your code here!
 
+// all these items are from 'styles.css' file which we are currently going to update/creat events
+
+// Only one section so we use "selector" without 'All'
 const currentColor = document.querySelector("#current-color")
 
 const color = document.querySelectorAll(".color")
-
+// these variable work in together because '#palette' is using the 'color' element
 const palette = document.querySelectorAll("#palette .color")
+
+// we are going to manipulate all the cells 
+const cell = document.querySelectorAll(".cell")
+
+color.forEach((newColor) => {
+  newColor.addEventListener('click',(event) => {
+    currentColor.style.backgroundColor = event.target.style.backgroundColor
+  })
+})
